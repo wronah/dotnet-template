6b80af2d04751ad8cf0c10d02d35359f6fadd9da
wronah <hubertwro7@gmail.com>
2025-06-10 14:59:26 +0200

    add user entity configuration

---
+++ dotnet-template/Template.Domain/Configurations/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Template.Domain.Entities;
+
+namespace Template.Domain.Configurations;
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(256);
+        builder.Property(u => u.LastName).HasMaxLength(256);
+    }
+}
