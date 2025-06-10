c62b8dcad4f8b1e1a77634f3976bafdafce16fed
wronah <hubertwro7@gmail.com>
2025-06-10 14:46:56 +0200

    add login and register requests

---
+++ dotnet-template/Template.Domain/Requests/RegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Template.Domain.Requests;
+public record RegisterRequest
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
