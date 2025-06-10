0345837b5640980a2cb08fda8a645d25adb85af6
wronah <hubertwro7@gmail.com>
2025-06-10 15:00:04 +0200

    add dependencies, configure dbcontext and configurations from assembly

---
+++ dotnet-template/Template.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using System.Reflection;
+using Template.Domain.Entities;
+
+namespace Template.Infrastructure;
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
